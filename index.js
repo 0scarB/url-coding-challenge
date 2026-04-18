3191ffc116bff4543b5bdd7a95e7ed8aeade201a
0scarB <oscarb@protonmail.com>
2026-04-18 15:16:11 +0200

    Update input status while typing

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
+"use strict";
 var UrlCheckState;
 (function (UrlCheckState) {
-    UrlCheckState[UrlCheckState["INVALID_FORMAT"] = 1] = "INVALID_FORMAT";
-    UrlCheckState[UrlCheckState["POINTS_TO_FILE"] = 2] = "POINTS_TO_FILE";
-    UrlCheckState[UrlCheckState["POINTS_TO_DIR"] = 3] = "POINTS_TO_DIR";
+    UrlCheckState[UrlCheckState["EMPTY_URL"] = 0] = "EMPTY_URL";
+    UrlCheckState[UrlCheckState["NOT_HTTP_OR_HTTPS"] = 1] = "NOT_HTTP_OR_HTTPS";
+    UrlCheckState[UrlCheckState["INVALID_FORMAT"] = 2] = "INVALID_FORMAT";
+    UrlCheckState[UrlCheckState["POINTS_TO_FILE"] = 3] = "POINTS_TO_FILE";
+    UrlCheckState[UrlCheckState["POINTS_TO_DIR"] = 4] = "POINTS_TO_DIR";
 })(UrlCheckState || (UrlCheckState = {}));
 var inputEl = document.getElementById("url-input");
 var successStatusMsgEl = document.getElementById("url-input-success-status-msg");
-function displayStatus(state) {
-    // Reset UI
-    inputEl.setCustomValidity("");
-    successStatusMsgEl.textContent = "";
-    // Update UI
-    switch (state) {
-        case UrlCheckState.INVALID_FORMAT:
-            console.log(state);
-            inputEl.setCustomValidity("Invalid URL format!");
-            break;
-        case UrlCheckState.POINTS_TO_FILE:
-            successStatusMsgEl.textContent = "URL points to file.";
-            break;
-        case UrlCheckState.POINTS_TO_DIR:
-            successStatusMsgEl.textContent = "URL points to folder.";
-            break;
-    }
-    console.log(inputEl.checkValidity());
-    inputEl.reportValidity();
-}
+var state = UrlCheckState.EMPTY_URL;
 function update() {
-    //displayStatus(UrlCheckState.INVALID_FORMAT);
-    //displayStatus(UrlCheckState.POINTS_TO_FILE);
-    displayStatus(UrlCheckState.POINTS_TO_DIR);
+    var url = inputEl.value;
+    updateState: {
+        //
+        // Check URL Format
+        //
+        try {
+            var urlObj = new URL(url);
+            if (urlObj.protocol !== "https:" && urlObj.protocol !== "http:") {
+                state = UrlCheckState.NOT_HTTP_OR_HTTPS;
+                break updateState;
+            }
+        }
+        catch (error) {
+            state = UrlCheckState.INVALID_FORMAT;
+            break updateState;
+        }
+        // TODO: await fetch request
+        state = UrlCheckState.POINTS_TO_FILE;
+    }
+    updateUI: {
+        // Reset UI
+        inputEl.setCustomValidity("");
+        successStatusMsgEl.textContent = "";
+        // Update UI
+        switch (state) {
+            case UrlCheckState.NOT_HTTP_OR_HTTPS:
+                inputEl.setCustomValidity("URL must start with 'https://' or 'http://'!");
+                break;
+            case UrlCheckState.INVALID_FORMAT:
+                inputEl.setCustomValidity("Invalid URL format!");
+                break;
+            case UrlCheckState.POINTS_TO_FILE:
+                successStatusMsgEl.textContent = "URL points to file.";
+                break;
+            //case UrlCheckState.POINTS_TO_DIR:
+            //    successStatusMsgEl.textContent = "URL points to folder.";
+            //    break;
+        }
+        inputEl.reportValidity();
+    }
 }
 inputEl.addEventListener("input", update);
 update();
--- index.ts
+++ index.ts
@@ -1,37 +1,61 @@
+"use strict";
+
 enum UrlCheckState {
-    INVALID_FORMAT = 1,
-    POINTS_TO_FILE = 2,
-    POINTS_TO_DIR  = 3,
+    EMPTY_URL = 0,
+    NOT_HTTP_OR_HTTPS,
+    INVALID_FORMAT,
+    POINTS_TO_FILE,
+    POINTS_TO_DIR,  
 }
 
 const inputEl            = document.getElementById("url-input") as HTMLInputElement;
 const successStatusMsgEl = document.getElementById("url-input-success-status-msg");
 
-function displayStatus(state: UrlCheckState) {
-    // Reset UI
-    inputEl.setCustomValidity("");
-    successStatusMsgEl.textContent = "";
-    // Update UI
-    switch (state) {
-        case UrlCheckState.INVALID_FORMAT:
-            console.log(state);
-            inputEl.setCustomValidity("Invalid URL format!");
-            break;
-        case UrlCheckState.POINTS_TO_FILE:
-            successStatusMsgEl.textContent = "URL points to file.";
-            break;
-        case UrlCheckState.POINTS_TO_DIR:
-            successStatusMsgEl.textContent = "URL points to folder.";
-            break;
-    }
-    console.log(inputEl.checkValidity());
-    inputEl.reportValidity();
-}
+let state: UrlCheckState = UrlCheckState.EMPTY_URL;
 
 function update() {
-    //displayStatus(UrlCheckState.INVALID_FORMAT);
-    //displayStatus(UrlCheckState.POINTS_TO_FILE);
-    displayStatus(UrlCheckState.POINTS_TO_DIR);
+    const url = inputEl.value;
+
+    updateState: {
+        //
+        // Check URL Format
+        //
+        try {
+            let urlObj = new URL(url);
+            if (urlObj.protocol !== "https:" && urlObj.protocol !== "http:") {
+                state = UrlCheckState.NOT_HTTP_OR_HTTPS;
+                break updateState;
+            }
+        } catch (error) {
+            state = UrlCheckState.INVALID_FORMAT;
+            break updateState;
+        }
+
+        // TODO: await fetch request
+        state = UrlCheckState.POINTS_TO_FILE;
+    }
+
+    updateUI: {
+        // Reset UI
+        inputEl.setCustomValidity("");
+        successStatusMsgEl.textContent = "";
+        // Update UI
+        switch (state) {
+            case UrlCheckState.NOT_HTTP_OR_HTTPS:
+                inputEl.setCustomValidity("URL must start with 'https://' or 'http://'!");
+                break;
+            case UrlCheckState.INVALID_FORMAT:
+                inputEl.setCustomValidity("Invalid URL format!");
+                break;
+            case UrlCheckState.POINTS_TO_FILE:
+                successStatusMsgEl.textContent = "URL points to file.";
+                break;
+            //case UrlCheckState.POINTS_TO_DIR:
+            //    successStatusMsgEl.textContent = "URL points to folder.";
+            //    break;
+        }
+        inputEl.reportValidity();
+    }
 }
 
 inputEl.addEventListener("input", update);
